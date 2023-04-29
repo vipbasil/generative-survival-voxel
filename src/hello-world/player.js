aa6a3afce2d1a2f8d98fa3e63ac4c2208e49a6c6
vipbasil <vipbasil@gmail.com>
2023-04-29 12:15:55 +0300

    added player.js

---
+++ src/hello-world/player.js
@@ -0,0 +1,65 @@
+
+
+// add a mesh to represent the player, and scale it, etc.
+import { Mesh } from '@babylonjs/core/Meshes/mesh'
+import '@babylonjs/core/Meshes/Builders/boxBuilder'
+export function initPlayerMesh(noa){
+var player = noa.playerEntity
+var dat = noa.entities.getPositionData(player)
+var w = dat.width
+var h = dat.height
+var scene = noa.rendering.getScene()
+var mesh = Mesh.CreateBox('player-mesh', 1, scene)
+mesh.scaling.x = w
+mesh.scaling.z = w
+mesh.scaling.y = h
+
+
+// add "mesh" component to the player entity
+// this causes the mesh to move around in sync with the player entity
+noa.entities.addComponent(player, noa.entities.names.mesh, {
+    mesh: mesh,
+    // offset vector is needed because noa positions are always the 
+    // bottom-center of the entity, and Babylon's CreateBox gives a 
+    // mesh registered at the center of the box
+    offset: [0, h / 2, 0],
+})
+ 
+
+
+/*
+ * 
+ *      Minimal interactivity 
+ * 
+*/
+
+// clear targeted block on on left click
+noa.inputs.down.on('fire', function () {
+    if (noa.targetedBlock) {
+        var pos = noa.targetedBlock.position
+        noa.setBlock(0, pos[0], pos[1], pos[2])
+    }
+})
+
+// place some grass on right click
+noa.inputs.down.on('alt-fire', function () {
+    if (noa.targetedBlock) {
+        var pos = noa.targetedBlock.adjacent
+        noa.setBlock(grassID, pos[0], pos[1], pos[2])
+    }
+})
+
+// add a key binding for "E" to do the same as alt-fire
+noa.inputs.bind('alt-fire', 'E')
+
+
+// each tick, consume any scroll events and use them to zoom camera
+noa.on('tick', function (dt) {
+    var scroll = noa.inputs.state.scrolly
+    if (scroll !== 0) {
+        noa.camera.zoomDistance += (scroll > 0) ? 1 : -1
+        if (noa.camera.zoomDistance < 0) noa.camera.zoomDistance = 0
+        if (noa.camera.zoomDistance > 10) noa.camera.zoomDistance = 10
+    }
+})
+}
